c706cc98b49d192fcd29fa92f007f3e4be6f2bac
Aniruddha Varma <Aniruddha Varma>
2016-08-19 09:05:43 +0530

    1. Convert input field (base64 string) to Image and display 2. Load an image from disk, convert to base64 string, and display the string

---
+++ WpfBase64Image_FromAndTo/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfBase64Image_FromAndTo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
